36f3afe7289e06b55cfe09e1641d92a5630aab3a
pranitmane <manepranit10@gmail.com>
2024-07-12 15:25:14 +0530

    added sys audio control support + bug fix

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import { WebSocketServer, WebSocket } from 'ws'
 import cors from 'cors'
 import http from 'http'
 import getIPAddress from './utils/myip'
+import setSystemVolume from './utils/setVolume'
 
 const app = express()
 const server = http.createServer(app)
 
 app.use(cors())
 app.use(express.static(__dirname + '/public'))
-console.log('dirname is:', __dirname)
 
 app.get('/', (req, res) => {
-    try{
+    try {
         res.sendFile(__dirname + '/public/index.html')
     } catch (error) {
         console.log(error)
@@ -30,23 +30,34 @@ server.listen(8080, () => {
 
 const wss = new WebSocketServer({ server: server });
 
-wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
-    ws.on('message', function message(data, isBinary) {
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                const jsonData = JSON.parse(data.toString());
-                if (jsonData.type === 'status') {
-                    console.log(jsonData)
-                    client.send(JSON.stringify(jsonData))
-                }
-                if (jsonData.type === 'command') {
-                    console.log(jsonData)
-                    client.send(JSON.stringify(jsonData))
-                }
-            }
-        });
-    });
+let previousStatus = ''
 
+wss.on('connection', function connection(ws) {
+    // let statusCount = 0
+
+    if (previousStatus !== '') ws.send(JSON.stringify({ type: 'init', data: previousStatus }))
+
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        const jsonData = JSON.parse(data.toString());
+        if (jsonData.type === 'sysCommand') {
+            setSystemVolume(jsonData.data.volume)
+        } else {
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN && client !== ws) {
+                    if (jsonData.type === 'status') {
+                        // console.log('status', statusCount++)
+                        previousStatus = jsonData.data
+                        client.send(JSON.stringify(jsonData))
+                    }
+                    if (jsonData.type === 'command') {
+                        client.send(JSON.stringify(jsonData))
+                    }
+                }
+            });
+        }
+    });
     console.log('connected clients:', wss.clients.size)
 });
+
